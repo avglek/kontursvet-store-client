e780fe8ac0d01eb44c68923cf0a1b46795a8bad0
Aleksandr Glek <avglek@gmail.com>
2025-10-18 08:22:25 +0300

    add button component

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {ButtonComponent} from '../../ui-kit/components/button/button.component';
+
+@Component({
+  selector: 'app-home',
+  imports: [
+    ButtonComponent
+  ],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
