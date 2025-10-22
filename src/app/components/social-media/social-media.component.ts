cdf3f32595d64fc8274b833e4b425a77daf3c9fa
Aleksandr Glek <avglek@gmail.com>
2025-10-22 06:48:43 +0300

    add header icon

--- src/app/components/social-media/social-media.component.ts
+++ src/app/components/social-media/social-media.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { SvgIconComponent } from 'angular-svg-icon';
 
 @Component({
   selector: 'app-social-media',
-  imports: [],
+  imports: [SvgIconComponent],
   templateUrl: './social-media.component.html',
-  styleUrl: './social-media.component.scss'
+  styleUrl: './social-media.component.scss',
 })
-export class SocialMediaComponent {
-
-}
+export class SocialMediaComponent {}
